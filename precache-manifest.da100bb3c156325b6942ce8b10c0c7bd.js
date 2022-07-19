ee8d88c68e1d4949a059f8eb00a6d102961dd6cd
Hayden Thai <thaihayden@gmail.com>
2022-07-18 18:33:15 -0700

    Updates

---
+++ precache-manifest.da100bb3c156325b6942ce8b10c0c7bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c1a22f728a7359aefa86",
+    "url": "/static/css/main.0c75bfd3.chunk.css"
+  },
+  {
+    "revision": "c1a22f728a7359aefa86",
+    "url": "/static/js/main.c1a22f72.chunk.js"
+  },
+  {
+    "revision": "14f65bc6ee90dc51b01c",
+    "url": "/static/js/1.14f65bc6.chunk.js"
+  },
+  {
+    "revision": "0ff28627bafd06094ab1",
+    "url": "/static/js/2.0ff28627.chunk.js"
+  },
+  {
+    "revision": "424ba75a3c324f48f3ac",
+    "url": "/static/js/runtime~main.424ba75a.js"
+  },
+  {
+    "revision": "d22cee7a6951cc0b1171419ca2860b6e",
+    "url": "/index.html"
+  }
+];
